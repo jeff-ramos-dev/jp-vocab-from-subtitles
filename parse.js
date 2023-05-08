391612b92801fc4f07084709750c40bd3f2309d5
Jeff Ramos <103221133+jeff-ramos-dev@users.noreply.github.com>
2023-05-08 06:07:47 -0700

    Cleanup

--- parse.js
+++ parse.js
@@ -1,7 +1,5 @@
 const fs = require('fs').promises;
 const kuromoji = require('kuromoji')
-const subtitleFile = './nausicaa.srt'
-const newFileLocation = './unique-wordlist-nausicaa.txt'
 
 async function readFile(filePath, outputFile) {
   try {
@@ -11,34 +9,34 @@ async function readFile(filePath, outputFile) {
     const japLines = []
     const regex = /[\\u3040-\\u30ff\\u3400-\\u4dbf\\u4e00-\\u9faf]/;
     for (let i = 0; i < numOfLines; i++) {
-        if (lines[i] && !regex.test(lines[i])) {
-            japLines.push(lines[i])
-        }
+      if (lines[i] && !regex.test(lines[i])) {
+        japLines.push(lines[i])
+      }
     }
     numOfLines = japLines.length
     kuromoji.builder({ dicPath: "./node_modules/kuromoji/dict/" }).build(function (err, tokenizer) {
-        const japWordsMany = []
-        const japWordsFew = []
-        for (let i = 0; i < numOfLines; i++) {
-            japWordsMany.push(tokenizer.tokenize(japLines[i]));
-            numOfWords = japWordsMany[i].length
-            for (let j = 0; j < numOfWords; j++) {
-                if (japWordsMany[i][j].pos !== '記号'
-                && !japWordsFew.includes(japWordsMany[i][j].surface_form)
-                && japWordsMany[i][j].surface_form.length > 1) {
-                    japWordsFew.push(japWordsMany[i][j].surface_form)
-                }
-            }
+      const japWordsMany = []
+      const japWordsFew = []
+      for (let i = 0; i < numOfLines; i++) {
+        japWordsMany.push(tokenizer.tokenize(japLines[i]));
+        numOfWords = japWordsMany[i].length
+        for (let j = 0; j < numOfWords; j++) {
+          if (japWordsMany[i][j].pos !== '記号'
+            && !japWordsFew.includes(japWordsMany[i][j].surface_form)
+            && japWordsMany[i][j].surface_form.length > 1) {
+            japWordsFew.push(japWordsMany[i][j].surface_form)
+          }
         }
-        numOfWords = japWordsFew.length
-        for (let i = 0; i < numOfWords; i++) {
-            japWordsFew[i] = `${i + 1}: ${japWordsFew[i]} \n`
+      }
+      numOfWords = japWordsFew.length
+      for (let i = 0; i < numOfWords; i++) {
+        japWordsFew[i] = `${i + 1}: ${japWordsFew[i]} \n`
+      }
+      fs.writeFile(outputFile, japWordsFew, err => {
+        if (err) {
+          console.error(err);
         }
-        fs.writeFile(outputFile,japWordsFew, err => {
-            if (err) {
-                console.error(err);
-            }
-        })
+      })
     });
   } catch (error) {
     console.error(`Got an error trying to read the file: ${error.message}`);
